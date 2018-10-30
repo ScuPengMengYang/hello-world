93afccd7d1df4ebd51870c80166a3458da982e7d
Scupengmengyang <578487960@qq.com>
2018-10-30 19:08:31 +0800

    equation1 add commit

--- equation1.c
+++ equation1.c
@@ -1,3 +1,14 @@
-//æ±‚è§£ä¸€å…ƒäºŒæ¬¡æ–¹ç¨‹çš„æ ¹
-//æŸ¥é˜…sqrt()å‡½æ•°
-//æ³¨æ„æœ‰æ²¡æœ‰è§£
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	float a,b,c;
+	float x1,x2,m;
+	printf("Çó½âÏÂÁĞ·½³Ì\nax^2+bx+c=0\nÇëÒÀ´ÎÊäÈëa,b,cµÄÖµ\n");
+	scanf("%f%f%f",&a,&b,&c);
+	m=b*b-4*a*c;
+	x1=(-b+sqrt(m))/(2*a);
+	x2=(-b-sqrt(m))/(2*a);
+	printf("x1 = %f\nx2 = %f\n",x1,x2);
+	return 0;
+} 
